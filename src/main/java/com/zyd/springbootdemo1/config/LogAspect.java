73f610d5e0d557e7c042d611b1dcc4142a34a583
blackCat <844241256@qq.com>
2022-10-24 12:02:10 +0800

    日志 aop

---
+++ src/main/java/com/zyd/springbootdemo1/config/LogAspect.java
@@ -0,0 +1,85 @@
+package com.zyd.springbootdemo1.config;
+
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@EnableAspectJAutoProxy
+@Component
+@Aspect
+public class LogAspect {
+
+    private static final Logger LOGGER= LoggerFactory.getLogger(LogAspect.class);
+    /**
+     * define point cut.
+     */
+    @Pointcut("execution(* com.zyd.springbootdemo1.service..*.*(..))")
+    private void pointCutMethod() {
+    }
+
+
+    /**
+     * 环绕通知.
+     *
+     * @param pjp pjp
+     * @return obj
+     * @throws Throwable exception
+     */
+    @Around("pointCutMethod()")
+    public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
+        LOGGER.info("-----------------------");
+        LOGGER.info("环绕通知: 进入方法");
+        Object o = pjp.proceed();
+        LOGGER.info("环绕通知: 退出方法");
+        return o;
+    }
+
+    /**
+     * 前置通知.
+     */
+    @Before("pointCutMethod()")
+    public void doBefore(JoinPoint joinPoint) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        LOGGER.info("url={},method={},ip={},class_method={},args={}", request.getRequestURI(),request.getMethod(),request.getRemoteAddr(),joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName(),joinPoint.getArgs());
+    }
+
+
+    /**
+     * 后置通知.
+     *
+     * @param result return val
+     */
+    @AfterReturning(pointcut = "pointCutMethod()", returning = "result")
+    public void doAfterReturning(String result) {
+        LOGGER.info("后置通知, 返回值: " + result);
+    }
+
+    /**
+     * 异常通知.
+     *
+     * @param e exception
+     */
+    @AfterThrowing(pointcut = "pointCutMethod()", throwing = "e")
+    public void doAfterThrowing(Exception e) {
+        LOGGER.info("异常通知, 异常: " + e.getMessage());
+    }
+
+    /**
+     * 最终通知.
+     */
+    @After("pointCutMethod()")
+    public void doAfter() {
+        LOGGER.info("最终通知");
+    }
+
+}
